cf06ef31e4b417c8d1c6b373547c20d0e7b999f9
kylehwelch <welch@udel.edu>
2022-07-25 11:33:37 -0400

    all tests passing

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -1,5 +1,6 @@
 package io.zipcoder.currencyconverterapplication;
 
+
 public enum CurrencyType {
     AUSTRALIAN_DOLLAR(2.70),
     CANADIAN_DOLLAR(2.64),
@@ -25,6 +26,32 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
+        switch (currency.getClass().getSimpleName()){
+            case "AustralianDollar":
+                return CurrencyType.AUSTRALIAN_DOLLAR;
+            case "CanadianDollar":
+                return CurrencyType.CANADIAN_DOLLAR;
+            case "ChineseYR":
+                return CurrencyType.CHINESE_YR;
+            case "Euro":
+                return CurrencyType.EURO;
+            case "Franc":
+                return CurrencyType.FRANC;
+            case "Pound":
+                return CurrencyType.POUND;
+            case "Ringgit":
+                return CurrencyType.RINGGIT;
+            case "Rupee":
+                return CurrencyType.RUPEE;
+            case "SingaporeDollar":
+                return CurrencyType.SINGAPORE_DOLLAR;
+            case "UniversalCurrency":
+                return CurrencyType.UNIVERSAL_CURRENCY;
+            case "USDollar":
+                return CurrencyType.US_DOLLAR;
+            case "Yen":
+                return CurrencyType.YEN;
+        }
         return null;
     }
 }
